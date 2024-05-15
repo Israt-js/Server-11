e15c96755ed682d8026fb058cb8af29964c83ad9
israt.js <israt8291jahan@gmail.com>
2024-05-15 20:19:17 +0600

    fix

--- index.js
+++ index.js
@@ -86,6 +86,12 @@ async function run() {
       const result = await assignCollection.update(filter, assign, options);
       res.send(result)
       })
+      app.delete('/createAssign/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await assignCollection.deleteOne(query);
+        res.send(result)
+      })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
